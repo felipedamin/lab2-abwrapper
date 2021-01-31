a4de61a1e1d490f16a9d6b8e94c1486e5af21a1f
Felipe A S Damin <felipes.damin96@gmail.com>
2021-01-31 15:32:53 -0300

    folder

---
+++ wrapper/queue.rs
@@ -0,0 +1,7 @@
+// array de dados
+// fetch test has priority
+// postEvent works in two ways
+    // it either retry if fails
+    // or put back on the end of the queue if it fails
+
+// how to handle concurrency?
