cf956fc39f0ab97c6efabdbd99e76a91c1600d7c
fentanes96 <lucas_fentanes@hotmail.com>
2021-04-05 23:44:09 -0300

    Job scheduler for automatically running jobs to update the aggregations.

--- statistics_backend/server_config.py
+++ statistics_backend/server_config.py
@@ -1,6 +1,26 @@
 import os
 from route_config import *
+from utils import get_config
+import psycopg2
+
+from flask_apscheduler import APScheduler
+
+def update_aggs():
+    conf = get_config()
+    conn = psycopg2.connect(**conf)
+    cur = conn.cursor()
+    cur.execute('call db_event_store.update_aggs();')
+    conn.commit()
+    cur.close()
+    conn.close()
+
 app.debug = True
 host = os.environ.get('IP', '0.0.0.0')
 port = int(os.environ.get('PORT', 8080))
+
+# Scheduler
+scheduler = APScheduler()
+scheduler.add_job(func=update_aggs, trigger='interval', id='job', seconds=60*10)
+scheduler.start()
+
 app.run(host=host, port=port)
